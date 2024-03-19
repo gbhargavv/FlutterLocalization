988e487c1d9186e342f856bc3ad411a745e9138e
Bhargav <bitbucketcode@ntechinfoway.com>
2024-03-19 18:11:43 +0530

    Revert "Init Commit"
    This reverts commit 1643556441c1ddaa74dde1c381779e9245177d5e.

--- EpicoPro/cpico_pro/lib/utils/ApiConstant.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstant {
-  static const String BASE_URL = 'https://api.epicopro.com/v1/';
-  static const String login = 'auth_token';
-  static const String stores = 'stores';
-  static const String product_search_link = 'generate_product_search_link';
-  static String token = '';
-}
--- EpicoPro/cpico_pro/lib/utils/constant.dart
+++
@@ -1,4 +0,0 @@
-class Constant {
-  static const String FONT_FAMILY = 'Mont';
-  static const String TOKEN = 'api_token';
-}
